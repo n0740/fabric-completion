805a8844312ef65a224891650515c6e45768dce5
Konstantin Bakulin <mail@kbakulin.com>
2011-10-02 23:55:33 +0400

    Update cache if fabfile was changed.

--- fabric-completion.bash
+++ fabric-completion.bash
@@ -12,6 +12,44 @@ export FAB_COMPLETION_CACHE_TASKS=1
 export FAB_COMPLETION_CACHED_TASKS_FILENAME=".fab_tasks~"
 
 
+#
+# Return time of last file modification as seconds since Epoch
+# TODO: check on Linux
+#
+function __fab_file_mtime() {
+    local file=$1
+    case `uname` in
+        Darwin|FreeBSD)
+            echo `stat -f '%m' $file`
+            ;;
+        *)
+            echo `stat -c '%Y' $file`
+            ;;
+    esac
+}
+
+
+#
+# Return time of last fab cache file modification as seconds since Epoch
+# TODO: use __fab_file_mtime() to get mtime
+#
+function __fab_chache_mtime() {
+    __fab_file_mtime $FAB_COMPLETION_CACHED_TASKS_FILENAME
+}
+
+
+# Return time of last fabfile file/module modification as seconds since Epoch
+function __fab_fabfile_mtime() {
+    local f="fabfile"
+    if [[ -e "$f.py" ]]; then
+        __fab_file_mtime "$f.py"
+    else
+        # Suppose it's a fabfile dir
+        find $f/*.py -exec stat -f '%m' {} + | sort -n -r | head -1
+    fi
+}
+
+
 function __fab_completion() {
     # Return if
     # - fab command doesn't exists
@@ -50,8 +88,8 @@ function __fab_completion() {
             # Build a list of the available tasks
             if [[ $FAB_COMPLETION_CACHE_TASKS -eq 1 ]]; then
                 # If use cache
-                if [[ ! -s ${FAB_COMPLETION_CACHED_TASKS_FILENAME} ]]; then
-                    # File.mtime(cache_file) >= File.mtime(rakefile)
+                if [[ ! -s ${FAB_COMPLETION_CACHED_TASKS_FILENAME} || $(__fab_fabfile_mtime) -gt $(__fab_chache_mtime) ]]; then
+                    echo "init cache"
                     fab --shortlist > ${FAB_COMPLETION_CACHED_TASKS_FILENAME}
                 fi
                 opts=$(cat ${FAB_COMPLETION_CACHED_TASKS_FILENAME})
